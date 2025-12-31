a175f23c7bf83c6542573bb1646686f6bf865bcd
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-12-31 16:24:10 +0000

    hello world web server

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var tmpl = template.Must(template.New("hello").Parse(`
+<!DOCTYPE html>
+<html>
+<head><title>Hello</title></head>
+<body><h1>Hello, World!</h1></body>
+</html>`))
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	srv := &http.Server{Addr: ":8080"}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-quit
+		log.Println("Shutting down server...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Fatalf("Server forced to shutdown: %v", err)
+		}
+	}()
+
+	log.Println("Server is listening on :8080")
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("ListenAndServe(): %v", err)
+	}
+
+	log.Println("Server stopped")
+}
