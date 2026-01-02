17b9ded1aa22d4bf954b78fe00ab8e7bc8233b50
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-01-02 09:36:59 +0000

    Extract database init as it touches events and users

---
+++ services/database.go
@@ -0,0 +1,48 @@
+package services
+
+import (
+	"database/sql"
+	"os"
+	"path/filepath"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func InitDB() (*sql.DB, error) {
+	if err := os.MkdirAll("data", os.ModePerm); err != nil {
+		return nil, err
+	}
+
+	dbPath := filepath.Join("data", "state.db")
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return nil, err
+	}
+
+	createEventsTable := `
+	CREATE TABLE IF NOT EXISTS events (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		tag TEXT,
+		comment TEXT,
+		value TEXT,
+		createdAt TEXT
+	);
+	`
+
+	createUsersTable := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		username TEXT,
+		email TEXT
+	);
+	`
+
+	if _, err = db.Exec(createEventsTable); err != nil {
+		return nil, err
+	}
+	if _, err = db.Exec(createUsersTable); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
--- services/eventstore.go
+++ services/eventstore.go
@@ -2,8 +2,6 @@ package services
 
 import (
 	"database/sql"
-	"os"
-	"path/filepath"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -22,45 +20,6 @@ type EventStore interface {
 
 type SQLiteEventStore struct {
 	db *sql.DB
-}
-
-func InitDB() (*sql.DB, error) {
-	if err := os.MkdirAll("data", os.ModePerm); err != nil {
-		return nil, err
-	}
-
-	dbPath := filepath.Join("data", "state.db")
-	db, err := sql.Open("sqlite3", dbPath)
-	if err != nil {
-		return nil, err
-	}
-
-	createEventsTable := `
-	CREATE TABLE IF NOT EXISTS events (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		tag TEXT,
-		comment TEXT,
-		value TEXT,
-		createdAt TEXT
-	);
-	`
-
-	createUsersTable := `
-	CREATE TABLE IF NOT EXISTS users (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		username TEXT,
-		email TEXT
-	);
-	`
-
-	if _, err = db.Exec(createEventsTable); err != nil {
-		return nil, err
-	}
-	if _, err = db.Exec(createUsersTable); err != nil {
-		return nil, err
-	}
-
-	return db, nil
 }
 
 func NewEventStore(db *sql.DB) EventStore {
