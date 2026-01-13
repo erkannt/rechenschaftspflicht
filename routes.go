04ad9ad7213468a9e9fda36e958ba762a79984db
Your Name <you@example.com>
2026-01-13 16:38:07 +0000

    Curry middleware to protect routes that need user to be logged in

--- routes.go
+++ routes.go
@@ -10,27 +10,32 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func mustBeLoggedIn(h httprouter.Handle, auth authentication.Auth) httprouter.Handle {
-	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		if !auth.IsLoggedIn(r) {
-			http.Redirect(w, r, "/login", http.StatusFound)
-			return
+func mustBeLoggedIn(auth authentication.Auth) func(httprouter.Handle) httprouter.Handle {
+	return func(h httprouter.Handle) httprouter.Handle {
+		return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+			if !auth.IsLoggedIn(r) {
+				http.Redirect(w, r, "/login", http.StatusFound)
+				return
+			}
+			h(w, r, ps)
 		}
-		h(w, r, ps)
 	}
 }
+
 func addRoutes(
 	router *httprouter.Router,
 	eventStore eventstore.EventStore,
 	userStore userstore.UserStore,
 	auth authentication.Auth,
 ) {
+	requireLogin := mustBeLoggedIn(auth)
+
 	router.GET("/", handlers.LandingHandler(auth))
 	router.POST("/login", handlers.LoginPostHandler(userStore, auth))
 	router.GET("/login", handlers.LoginGetHandler(auth))
 	router.GET("/check-your-email", handlers.CheckYourEmailHandler)
-	router.GET("/record-event", mustBeLoggedIn(handlers.RecordEventFormHandler, auth))
-	router.POST("/record-event", mustBeLoggedIn(handlers.RecordEventPostHandler(eventStore, auth), auth))
-	router.GET("/all-events", mustBeLoggedIn(handlers.AllEventsHandler(eventStore), auth))
-	router.GET("/logout", mustBeLoggedIn(handlers.LogoutHandler, auth))
+	router.GET("/record-event", requireLogin(handlers.RecordEventFormHandler))
+	router.POST("/record-event", requireLogin(handlers.RecordEventPostHandler(eventStore, auth)))
+	router.GET("/all-events", requireLogin(handlers.AllEventsHandler(eventStore)))
+	router.GET("/logout", requireLogin(handlers.LogoutHandler))
 }
